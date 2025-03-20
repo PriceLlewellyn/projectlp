661d405e6d7390c63d10a73c5c259dc64592542c
Llewellyn Price <price13.llewellyn@gmail.com>
2025-03-20 12:39:10 +0000

    Arrays

--- helloworld.ts
+++ helloworld.ts
@@ -1,6 +1,22 @@
-const nothing = [{
+const students = [
+    {
     name: "Llewellyn",
     age: 25,
-    job: "Software Engineer"
-}]
-console.log(nothing)
+    job: "Software Engineer",
+    grades: {
+        maths:"c",
+        english:"d"
+    } 
+},
+{
+    name: "DAN",
+    age: 32,
+    job: "Software Beast",
+    grades: {
+        maths:"c",
+        english:"e"
+    }
+    
+}
+]
+console.log(students[1]["grades"]["maths"])
