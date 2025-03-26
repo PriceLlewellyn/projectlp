a668adbbd5cc00435eaafadd95c3feae76594e5d
Llewellyn Price <price13.llewellyn@gmail.com>
2025-03-26 13:56:49 +0000

    Sorted out Compiler

---
+++ practice.ts
@@ -0,0 +1,15 @@
+type dog = {
+    name: string;
+    age: number;
+    breed: string;
+    fights: boolean
+};
+
+let dog = {
+    name: "amber",
+    age: 13,
+    breed: "Malamute",
+    fights: true
+}
+
+console.log(dog.age)
